3b44e3ddc4a5a0c4a2bae0e79a13022c9a4cf59e
pranavmandal <pranavmandal@users.noreply.github.com>
2020-06-16 23:26:28 +0530

    Code file created.

---
+++ Visualization-for-company-stakeholders-using-matplotlib-/code.py
@@ -0,0 +1,93 @@
+# --------------
+#Importing header files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+data=pd.read_csv(path)
+loan_status = data['Loan_Status'].value_counts(ascending=False)
+print(loan_status)
+plt.figure(figsize=(18,4))
+loan_status.plot(kind='bar')
+plt.show()
+
+
+
+#Code starts here
+
+
+# --------------
+#Code starts here
+property_and_loan = data.groupby(['Property_Area','Loan_Status']).size().unstack()
+property_and_loan.plot(kind='bar',stacked=False,)
+plt.xlabel('Property Area')
+plt.ylabel('Loan Status')
+plt.xticks(rotation=45)
+
+plt.show()
+
+
+# --------------
+education_and_loan=data.groupby(['Education','Loan_Status']).size().unstack()
+education_and_loan.plot(kind='bar',stacked=False)
+plt.xlabel('Education Status')
+plt.ylabel('Loan_Status')
+plt.xticks(rotation=45)
+
+plt.show()
+
+
+# --------------
+#Code starts here
+
+
+
+
+
+
+
+#Code starts here
+
+#Subsetting the dataframe based on 'Education' column
+graduate=data[data['Education']=='Graduate']
+
+
+#Subsetting the dataframe based on 'Education' column
+not_graduate=data[data['Education']=='Not Graduate']
+
+
+#Plotting density plot for 'Graduate'
+graduate['LoanAmount'].plot(kind='density', label='Graduate')
+
+
+#Plotting density plot for 'Graduate'
+not_graduate['LoanAmount'].plot(kind='density',label='Not Graduate')
+
+
+#Code ends here
+
+#For automatic legend display
+plt.legend()
+
+
+
+
+
+#Code ends here
+
+#For automatic legend display
+plt.legend()
+
+
+# --------------
+#Code starts here
+fig,(ax_1,ax_2,ax_3)=plt.subplots(3,1,figsize=(14,8))
+ax_1.scatter(data['ApplicantIncome'],data['LoanAmount'])
+ax_1.set_title('Applicant Income')
+ax_2.scatter(data['CoapplicantIncome'],data['LoanAmount'])
+ax_2.set_title('Coapplicant Income')
+data['TotalIncome']=data['ApplicantIncome']+data['CoapplicantIncome']
+ax_3.scatter(data['TotalIncome'],data['LoanAmount'])
+ax_3.set_title('Total Income')
+plt.show()
+
+
