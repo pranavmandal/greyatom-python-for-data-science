28a5b97fa503db51ec1e997785889cdb610321e3
pranavmandal <60251074+pranavmandal@users.noreply.github.com>
2020-06-16 23:31:00 +0530

    Update code.py

--- Visualization-for-company-stakeholders-using-matplotlib-/code.py
+++ Visualization-for-company-stakeholders-using-matplotlib-/code.py
@@ -1,48 +1,59 @@
-# --------------
 #Importing header files
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+#Code starts here
+
+#Reading the file
 data=pd.read_csv(path)
-loan_status = data['Loan_Status'].value_counts(ascending=False)
-print(loan_status)
-plt.figure(figsize=(18,4))
-loan_status.plot(kind='bar')
+
+#Creating a new variable to store the value counts
+loan_status=data['Loan_Status'].value_counts()
+
+#Plotting bar plot
+plt.bar(loan_status.index, loan_status)
 plt.show()
 
+#Code ends here
 
 
 #Code starts here
 
+#Plotting an unstacked bar plot
+property_and_loan=data.groupby(['Property_Area', 'Loan_Status'])
+property_and_loan=property_and_loan.size().unstack()
+property_and_loan.plot(kind='bar', stacked=False, figsize=(15,10))
 
-# --------------
-#Code starts here
-property_and_loan = data.groupby(['Property_Area','Loan_Status']).size().unstack()
-property_and_loan.plot(kind='bar',stacked=False,)
-plt.xlabel('Property Area')
-plt.ylabel('Loan Status')
-plt.xticks(rotation=45)
+#Changing the x-axis label
+plt.xlabel('Property_Area')
 
-plt.show()
-
-
-# --------------
-education_and_loan=data.groupby(['Education','Loan_Status']).size().unstack()
-education_and_loan.plot(kind='bar',stacked=False)
-plt.xlabel('Education Status')
+#Changing the y-axis label
 plt.ylabel('Loan_Status')
+
+#Rotating the ticks of X-axis
 plt.xticks(rotation=45)
 
-plt.show()
+#Code ends here
 
 
-# --------------
 #Code starts here
 
+#Plotting a stacked bar plot
+education_and_loan=data.groupby(['Education', 'Loan_Status'])
+education_and_loan=education_and_loan.size().unstack()
+education_and_loan.plot(kind='bar', stacked=True, figsize=(15,10))
 
 
+#Changing the x-axis label
+plt.xlabel('Education Status')
 
+#Changing the y-axis label
+plt.ylabel('Loan Status')
 
+#Rotating the ticks of X-axis
+plt.xticks(rotation=45)
+
+#Code ends here
 
 
 #Code starts here
@@ -60,7 +71,7 @@ graduate['LoanAmount'].plot(kind='density', label='Graduate')
 
 
 #Plotting density plot for 'Graduate'
-not_graduate['LoanAmount'].plot(kind='density',label='Not Graduate')
+not_graduate['LoanAmount'].plot(kind='density',label='Not Graduate',)
 
 
 #Code ends here
@@ -68,26 +79,33 @@ not_graduate['LoanAmount'].plot(kind='density',label='Not Graduate')
 #For automatic legend display
 plt.legend()
 
-
-
-
-
-#Code ends here
-
-#For automatic legend display
-plt.legend()
-
-
-# --------------
 #Code starts here
-fig,(ax_1,ax_2,ax_3)=plt.subplots(3,1,figsize=(14,8))
-ax_1.scatter(data['ApplicantIncome'],data['LoanAmount'])
-ax_1.set_title('Applicant Income')
-ax_2.scatter(data['CoapplicantIncome'],data['LoanAmount'])
-ax_2.set_title('Coapplicant Income')
-data['TotalIncome']=data['ApplicantIncome']+data['CoapplicantIncome']
-ax_3.scatter(data['TotalIncome'],data['LoanAmount'])
-ax_3.set_title('Total Income')
-plt.show()
+
+#Setting up the subplots
+fig, (ax_1, ax_2,ax_3) = plt.subplots(1,3, figsize=(20,8))
+
+#Plotting scatter plot
+ax_1.scatter(data['ApplicantIncome'],data["LoanAmount"])
+
+#Setting the subplot axis title
+ax_1.set(title='Applicant Income')
 
 
+#Plotting scatter plot
+ax_2.scatter(data['CoapplicantIncome'],data["LoanAmount"])
+
+#Setting the subplot axis title
+ax_2.set(title='Coapplicant Income')
+
+
+#Creating a new column 'TotalIncome'
+data['TotalIncome']= data['ApplicantIncome']+ data['CoapplicantIncome']
+
+#Plotting scatter plot
+ax_3.scatter(data['TotalIncome'],data["LoanAmount"])
+
+#Setting the subplot axis title
+ax_3.set(title='Total Income')
+
+
+#Code ends here
